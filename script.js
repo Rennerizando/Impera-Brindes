8a4880421c13831b153612eccb495534a944f273
Joao Renner <101888075+Rennerizando@users.noreply.github.com>
2022-05-01 16:35:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -15,3 +15,18 @@ function toggleMenu(event) {
 
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
+
+$ (function(){
+  $('.thumbs img').click(function(){
+    var cover = $('.cover img');
+    var thumb = $(this).attr('src');
+    if(cover.attr('src') !== thumb){
+    cover.fadeTo('200', '0', function(){
+      cover.attr('src', thumb);
+      cover.fadeTo('150','1');
+    });
+    $('.thumbs img').removeClass('active');
+    $(this).addClass('active');
+  }
+  });
+});
